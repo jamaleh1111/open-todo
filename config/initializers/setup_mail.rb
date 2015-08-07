5710bd1e1bfdc4ae52f97d8491fdf996261a99f4
Jamaleh <Jamaleh@hotmail.com>
2015-08-07 21:38:45 +1000

    fix gemfile and add api controllers

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
-  actionMailer::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address:         'smtp.sendgrid.net',
     port:            '587',
     authentication:  :plain,
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/about'
 
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end 
+
   root to: 'welcome#index'
 end 
 
