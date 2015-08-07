6bb9529bffd4fe9f668b751de09eabee6f7b0b64
Jamaleh <Jamaleh@hotmail.com>
2015-08-07 21:52:57 +1000

    working on api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,10 +3,11 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   private
+  
   def authenticated?
-    authenticate_or_request_with_http_basic do |username, password| 
-      User.where( username: username, password: password).present? 
-    end
+
+    authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }
+
   end 
 
 end 
