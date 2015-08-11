7e9a99166ecd97ec76aaed165cf98dfc9160f077
Jamaleh <Jamaleh@hotmail.com>
2015-08-11 17:17:52 +1000

    add list and item  controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users 
   get 'welcome/about'
 
   namespace :api, defaults: { format: :json } do
-    resources :users
+    resources :users do 
+      resources :lists
   end 
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end 
+
+    resources :items, only: [:destroy]
+    
 
   root to: 'welcome#index'
 end 
