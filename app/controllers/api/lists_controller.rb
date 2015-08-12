81f7655ad6c57fb9e3fef20fd471d542749acc1c
Jamaleh <Jamaleh@hotmail.com>
2015-08-12 10:30:10 +1000

    add create and destroy methods

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -2,6 +2,27 @@ class Api::ListsController < ApiController
   before_action :authenticated?
 
   def create
+    list = List.new(list_params)
+    if list.save
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
   end 
+
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+
+      render json: {}, status: :not_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end 
+  end 
+
+  private
+  def list_params
+    params.require(:list).permit(:title)
 
 end 
