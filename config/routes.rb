6ddb45661301913ff02406fc6e5e7ac89f023866
Jamaleh <Jamaleh@hotmail.com>
2015-08-12 10:48:29 +1000

    add update method

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,24 @@ Rails.application.routes.draw do
 
   devise_for :users 
   get 'welcome/about'
+  root to: 'welcome#index'
+
 
   namespace :api, defaults: { format: :json } do
     resources :users do 
       resources :lists
   end 
 
+
     resources :lists, only: [] do
-      resources :items, only: [:create]
+      resources :items, only: [:create, :update]
     end 
 
-    resources :items, only: [:destroy]
+    resources :items, only: [:destroy, :update]
+  end 
+
     
 
-  root to: 'welcome#index'
 end 
 
   # The priority is based upon order of creation: first created -> highest priority.
